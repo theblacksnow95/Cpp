1939fe1e7aae7d030f35fbc8d709948ab424c402
theblacksnow95 <enrique.murillo24@gmail.com>
2026-03-13 16:47:31 +0100

    cpp07

---
+++ cpp07/ex00/src/main.cpp
@@ -0,0 +1,37 @@
+#include "functions.hpp"
+
+int main()
+{
+	std::cout << YLL << "Test from the subject" << RST << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << YLL << "Another test" << RST << std::endl;
+	{
+		float a,b;
+		a = 10.34 , b = 90;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "c";
+		std::string d = "a";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	}
+	return (0);
+}
