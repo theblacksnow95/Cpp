cc461fb93d4bf8e42f7481088eee3a29dfecc756
theblacksnow95 <enrique.murillo24@gmail.com>
2026-03-13 11:37:29 +0100

    cehcks

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -2,7 +2,6 @@
 
 ScalarConverter::ScalarConverter()
 {
-	// std::cout << "ScalarConverter Default constructor called" << std::endl;
 }
 
 ScalarConverter::ScalarConverter(const ScalarConverter& other)
@@ -23,7 +22,6 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 ScalarConverter::~ScalarConverter()
 {
-	// std::cout << "ScalarConverter Destructor called." << std::endl;
 }
 
 std::string	ScalarConverter::charCheck(std::string& literal)
@@ -41,7 +39,9 @@ std::string	ScalarConverter::intCheck(std::string& literal)
 
 	if (literal.length() <= 11 && !(literal.find_first_not_of("+-0123456789") != literal.npos))
 	{
-		if (literal.find("-") != literal.npos && literal.find("-") != 0)
+		if (literal.find_first_of('-') != literal.find_last_of('-') || literal.find_first_of('+') != literal.find_last_of('+'))
+			return (ERROR);
+		if ((literal.find("-") != literal.npos && literal.find("-")) != 0 || (literal.find("+") != literal.npos && literal.find("+") != 0))
 			return (ERROR);
 		if (atol(literal.c_str()) > INT_MAX || atol(literal.c_str()) < INT_MIN)
 			return (ERROR);
@@ -54,12 +54,19 @@ std::string	ScalarConverter::intCheck(std::string& literal)
 std::string	ScalarConverter::doubleCheck(std::string& literal)
 {
 	double tmp;
+	if (literal.find_first_of('-') != literal.find_last_of('-') || literal.find_first_of('+') != literal.find_last_of('+'))
+		return (ERROR);
+	if ((literal.find("-") != literal.npos && literal.find("-") != 0 ) || (literal.find("+") != literal.npos && literal.find("+") != 0))
+		return (ERROR);
 	if (literal.find("f") != literal.npos)
 		return (ERROR);
 	if (literal.find_first_not_of("+-0123456789.") !=  literal.npos)
 		return (ERROR);
-	tmp = strtod(literal.c_str(), NULL);
-	if (tmp != literal.npos)
+	if (!std::isdigit(literal[literal.find('.') + 1]) || literal.find('.') == 0)
+		return (ERROR);
+	char *end;
+	tmp = strtod(literal.c_str(), &end);
+	if (*end == '\0')
 	{
 		_valDouble = tmp;
 		return (DOUBLE);
@@ -70,16 +77,23 @@ std::string	ScalarConverter::doubleCheck(std::string& literal)
 
 std::string	ScalarConverter::floatCheck(std::string& literal)
 {
-	size_t sep_pos = literal.find(".");
-	if (literal.find("f") == literal.npos)
+	size_t sep_pos = literal.find('.');
+	size_t findic = literal.find('f');
+	if (literal.find_first_of('-') != literal.find_last_of('-') || literal.find_first_of('+') != literal.find_last_of('+'))
 		return (ERROR);
-	if ((literal.find_first_not_of("+-0123456789f.") != literal.npos))
+	if ((literal.find("-") != literal.npos && literal.find("-") != 0 ) || (literal.find("+") != literal.npos && literal.find("+") != 0))
 		return (ERROR);
-	if (sep_pos == literal.npos || sep_pos == 0 || sep_pos == literal.length() || literal.find_last_of(".") != sep_pos)
+	if (findic == literal.npos || findic != literal.find_last_of('f') || findic != literal.size() -1 )
+		return (ERROR);
+	if (literal.at(sep_pos + 1) == 'f')
+		return (ERROR);
+	if (literal.find_first_not_of("+-0123456789f.") != literal.npos)
+		return (ERROR);
+	if (sep_pos == literal.npos || sep_pos == 0 || sep_pos == literal.length() || literal.find_last_of('.') != sep_pos)
 		return (ERROR);
 	std::stringstream ss(literal);
 	float num;
-	num =  strtof(literal.c_str(), NULL);
+	ss >> num;
 	if (num != literal.npos)
 	{
 		_valFloat = num;
@@ -131,7 +145,7 @@ void	ScalarConverter::conversion(std::string& type, std::string& literal)
 		_valInt = static_cast<int>(_valChar);
 		std::cout << BLE << "int:\t" << _valInt << RST << std::endl;
 		_valFloat = static_cast<float>(_valChar);
-		std::cout << std::fixed << std::setprecision(2) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
+		std::cout << std::fixed << std::setprecision(1) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
 		_valDouble = static_cast<double>(_valChar);
 		std::cout << BLE << "double:\t" << _valDouble << RST << std::endl;
 	}
@@ -149,13 +163,13 @@ void	ScalarConverter::conversion(std::string& type, std::string& literal)
 			std::cout << BLE << "char:\t" << "impossible" << RST << std::endl;
 		std::cout << BLE << "int:\t" << _valInt << RST << std::endl;
 		_valFloat = static_cast<float>(_valInt);
-		std::cout << std::fixed << BLE << "float:\t" << std::setprecision(2) << _valFloat << "f" << RST << std::endl;
+		std::cout << std::fixed << BLE << "float:\t" << std::setprecision(1) << _valFloat << "f" << RST << std::endl;
 		_valDouble = static_cast<double>(_valInt);
-		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(2) << _valDouble << RST << std::endl;
+		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(1) << _valDouble << RST << std::endl;
 	}
 	if (type == FLOAT)
 	{
-		if (_valFloat <= 127 || _valFloat >= 0)
+		if (_valFloat <= 127 && _valFloat >= 0)
 		{
 			_valChar = static_cast<char>(_valFloat);
 			if (!isprint(_valChar))
@@ -170,9 +184,9 @@ void	ScalarConverter::conversion(std::string& type, std::string& literal)
 			std::cout << BLE << "int:\t" << "impossible" << RST << std::endl;
 		else
 			std::cout << BLE << "int:\t" << _valInt << RST << std::endl;
-		std::cout << std::fixed << std::setprecision(2) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
+		std::cout << std::fixed << std::setprecision(1) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
 		_valDouble = static_cast<double>(_valFloat);
-		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(2) << _valDouble << RST << std::endl;
+		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(1) << _valDouble << RST << std::endl;
 	}
 	if (type == DOUBLE)
 	{
@@ -192,8 +206,8 @@ void	ScalarConverter::conversion(std::string& type, std::string& literal)
 		else
 			std::cout << BLE << "int:\t" << _valInt << RST << std::endl;
 		_valFloat = static_cast<float>(_valDouble);
-		std::cout << std::fixed << std::setprecision(2) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
-		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(2) << _valDouble << RST << std::endl;
+		std::cout << std::fixed << std::setprecision(1) << BLE << "float:\t" << _valFloat << "f" << RST << std::endl;
+		std::cout << std::fixed << BLE << "double:\t" << std::setprecision(1) << _valDouble << RST << std::endl;
 	}
 	if (type == PSEUDO)
 	{
@@ -222,7 +236,13 @@ void	ScalarConverter::convert(std::string& literal)
 	std::string type;
 	ScalarConverter a = ScalarConverter();
 	type = a.identify(literal) ;
-	std::cout << YLL << type << RST << std::endl;
+	if (type == ERROR)
+	{
+		std::cout << RED << "Incorrect format, try again." << RST << std::endl;
+		return ;
+	}
+	std::cout << YLL << std::string(60, '-') << RST << std::endl;
 	a.conversion(type, literal);
+	std::cout << YLL << std::string(60, '-') << RST << std::endl;
 
 }
