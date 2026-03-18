6b860475d7713b388ccc43d5f61a124ac9f05b1e
theblacksnow95 <enrique.murillo24@gmail.com>
2026-03-18 19:04:56 +0100

    dxffds

---
+++ cpp08/ex00/include/colors.hpp
@@ -0,0 +1,11 @@
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+# define GRN	"\033[0;32m"
+# define YLL	"\033[0;33m"
+# define RED	"\033[0;31m"
+# define RST	"\033[0m"
+# define BLE	"\033[0;34m"
+# define PRL	"\033[0;35m"
+
+#endif
---
+++ cpp08/ex00/include/easyfind.hpp
@@ -0,0 +1,14 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+# include <iostream>
+# include "colors.hpp"
+
+template <template <typename> typename T>
+int	easyfind(T<int> cont, int target)
+{
+	if (cont.at())
+}
+
+
+#endif
